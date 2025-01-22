cd793324f28aa8e52ab2a7478f33dbd41d8d4dbf
Gan Shen <gan_shen@icloud.com>
2025-01-22 01:28:32 -0800

    finish `project`

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,6 @@ import Control.Concurrent.Async
 import Control.CSD.Site
 import Control.CSD.CSD
 import System.Environment
-import Control.Concurrent
 
 priceOf :: String -> Int
 priceOf "TaPL" = 80
@@ -31,7 +30,7 @@ bookstore1 :: (CSD f) => f (Site String, Site ()) (Site Int, Site ())
 bookstore1 =
       (perf (\s -> return ((), s)) >>> fork) *** noop
   >>> assocR
-  >>> noop *** (joinL >>> perf (\(t, _) -> return t))
+  >>> noop *** (joinL >>> perf (\(_, t) -> return t))
   >>> noop *** perf (\t -> return (priceOf t, ()))
   >>> noop *** fork
   >>> assocL
@@ -103,29 +102,50 @@ main = do
       (s1', s2') <- runCSD bookstore1 (s1, s2)
       s1'' <- async (wait s1' >>= print)
       mapM_ wait [s1'', s2']
-    -- ["bookstore1", "buyer"] -> do
-    --   s1 <- async (putStrLn "Type in the title of the book:" >> getLine)
-    --   s2 <- return empty
-    --   let prog = project bookstore1 (Conj Self (Peer "Seller")) runKleisli
-    --   (_, (s1', s2')) <- prog (s1, s2)
-    --   s1'' <- async (wait s1' >>= print)
-    --   mapM_ wait [s1'', s2']
-    -- ["bookstore1", "seller"] -> do
-    --     s1 <- return empty
-    --     s2 <- async (return ())
-    --     let prog = project bookstore1 (Conj (Peer "Buyer") Self) runKleisli
-    --     (_, (s1', s2')) <- prog (s1, s2)
-    --     mapM_ wait [s2']
-    --     threadDelay 5_000_000
+    ["bookstore1", "buyer"] -> do
+      s1 <- async (putStrLn "Type in the title of the book:" >> getLine)
+      s2 <- async (return ())
+      (Here s1, There _ s2) <- project bookstore1 (Here s1, There "2.2.2.2:8080" s2)
+      s1 <- async (wait s1 >>= print)
+      mapM_ wait [s1, s2]
+    ["bookstore1", "seller"] -> do
+      s1 <- async (return ())
+      s2 <- async (return ())
+      (There _ s1, Here s2) <- project bookstore1 (There "1.1.1.1:8080" s1, Here s2)
+      mapM_ wait [s1, s2]
     ["bookstore2"] -> do
       s11 <- async (putStrLn "Buyer 1: Type in the title of the book:" >> getLine)
       s12 <- async (return ())
       s21 <- async (putStrLn "Buyer 2: Type in the title of the book:" >> getLine)
       s22 <- async (return ())
-      ((s11', s12'), (s21', s22')) <- runCSD bookstore2 ((s11, s12), (s21, s22))
-      s11'' <- async (wait s11' >>= print)
-      s21'' <- async (wait s21' >>= print)
-      mapM_ wait [s11'', s12', s21'', s22']
+      ((s11, s12), (s21, s22)) <- runCSD bookstore2 ((s11, s12), (s21, s22))
+      s11 <- async (wait s11 >>= print)
+      s21 <- async (wait s21 >>= print)
+      mapM_ wait [s11, s12, s21, s22]
+    ["bookstore2", "buyer1"] -> do
+      s11 <- async (putStrLn "Buyer 1: Type in the title of the book:" >> getLine)
+      s12 <- async (return ())
+      s21 <- async (return ())
+      s22 <- async (return ())
+      ((Here s11, There _ s12), (There _ s21, There _ s22)) <- project bookstore2 ((Here s11, There "3.3.3.3" s12), (There "2.2.2.2" s21, There "3.3.3.3" s22))
+      s11 <- async (wait s11 >>= print)
+      mapM_ wait [s11, s12, s21, s22]
+    ["bookstore2", "buyer2"] -> do
+      s11 <- async (return ())
+      s12 <- async (return ())
+      s21 <- async (putStrLn "Buyer 2: Type in the title of the book:" >> getLine)
+      s22 <- async (return ())
+      ((There _ s11, There _ s12), (Here s21, There _ s22)) <- project bookstore2 ((There "1.1.1.1" s11, There "3.3.3.3" s12), (Here s21, There "3.3.3.3" s22))
+      s21 <- async (wait s21 >>= print)
+      mapM_ wait [s11, s12, s21, s22]
+    ["bookstore2", "seller"] -> do
+      s11 <- async (return ())
+      s12 <- async (return ())
+      s21 <- async (return ())
+      s22 <- async (return ())
+      ((There _ s11, Here s12), (There _ s21, Here s22)) <- project bookstore2 ((There "1.1.1.1" s11, Here s12), (There "2.2.2.2" s21, Here s22))
+      mapM_ wait [s11, s12, s21, s22]
+
     -- ["bookstore3"] -> do
     --   s1 <- async (putStrLn "Type in the title of the book:" >> getLine)
     --   s2 <- async (return ())
--- src/Control/CSD/CSD.hs
+++ src/Control/CSD/CSD.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Control.CSD.CSD where
 
@@ -12,20 +13,24 @@ class (Perm f) => CSD f where
 
   -- Parallel Composition
   par :: f a c -> f b d -> f (a, b) (c, d)
+  -- fork/join are right-biased, meaning the right one is the outcoming/incoming site. In some
+  -- cases, which one is the outcoming/incoming site has no effect on the semantics, but if it
+  -- does, consider use directful join/fork and give them specific semantics rather than using
+  -- the default one.
   fork :: f (Site (a, b)) (Site a, Site b)
-  join :: f (Site a, Site b) (Site (a, b))
+  join :: (Show b, Read b) => f (Site a, Site b) (Site (a, b))
 
   -- Directful fork/join
-  forkL :: f (Site (a, b)) (Site a, Site b)
-  forkL = fork
+  forkL :: f (Site (a, out)) (Site out, Site a)
+  forkL = fork >>> swap
 
-  forkR :: f (Site (a, b)) (Site a, Site b)
+  forkR :: f (Site (a, out)) (Site a, Site out)
   forkR = fork
 
-  joinL :: f (Site a, Site b) (Site (a, b))
-  joinL = join
+  joinL :: (Show in', Read in') => f (Site in', Site b) (Site (b, in'))
+  joinL = swap >>> join
 
-  joinR :: f (Site a, Site b) (Site (a, b))
+  joinR :: (Show in', Read in') => f (Site a, Site in') (Site (a, in'))
   joinR = join
 
 -- the following operators are named after arrow operators share the same behavior
@@ -44,13 +49,49 @@ a *** b = par a b
 
 -- ** Centralized Semantics
 
-instance (CSD (Tmap Async IO)) where
+instance CSD (Tmap Async IO) where
   perf act = Tmap $ \a -> async (wait a >>= act)
   seqq f g = Tmap $ \a -> runTmap f a >>= runTmap g
-  par f g  = Tmap $ \(a, b) -> (,) <$> runTmap f a <*> runTmap g b
+  par  f g = Tmap $ \(a, b) -> (,) <$> runTmap f a <*> runTmap g b
   fork     = Tmap $ \ab -> (,) <$> async (fst <$> wait ab) <*> async (snd <$> wait ab)
   join     = Tmap $ \(a, b) -> async ((,) <$> wait a <*> wait b)
 
 -- I really want to type this `(CSD f) => f a b -> T Async a -> IO (T Async b)`
 runCSD :: Tmap Async IO a b -> T Async a -> IO (T Async b)
 runCSD = runTmap
+
+-- ** Distributed Semantics
+
+-- Backend function
+type Addr = String
+
+send :: (Show a) => Addr -> a -> IO ()
+send addr a = putStrLn (">>> send " ++ show a ++ " to " ++ addr)
+
+recv :: (Read a) => Addr -> IO a
+recv addr = putStrLn (">>> recv from " ++ addr) >> (read <$> getLine)
+
+data Located a where
+  Here :: Async a -> Located a
+  There :: Addr -> Async () -> Located a
+
+instance CSD (Tmap Located IO) where
+  perf act = Tmap $ \case
+    (Here a) -> Here <$> async (wait a >>= act)
+    (There addr a) -> return (There addr a)
+  -- the following two cases are exactly the same as the centralized semantics, which might mean
+  -- we can futher refactor out things
+  seqq f g = Tmap $ \a -> runTmap f a >>= runTmap g
+  par  f g = Tmap $ \(a, b) -> (,) <$> runTmap f a <*> runTmap g b
+  fork = Tmap $ \case
+    (Here ab) -> (,) <$> (Here <$> async (fst <$> wait ab)) <*> (Here <$> async (snd <$> wait ab))
+    (There addr a) -> return (There addr a, There addr a)
+  join = Tmap $ \case
+    (Here a, Here b) -> Here <$> async ((,) <$> wait a <*> wait b)
+    (Here a, There addr _) -> Here <$> async ((,) <$> wait a <*> recv addr)
+    (There addr a, Here b) -> There addr <$> async (wait a >> wait b >>= send addr)
+    (There addr1 a, There _ _) -> return (There addr1 a)
+
+-- same as above, I don't like the type signature
+project :: Tmap Located IO a b -> T Located a -> IO (T Located b)
+project = runTmap
